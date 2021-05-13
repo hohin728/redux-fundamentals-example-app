5e1c2973254c1ef2037bda795c187fd45519c877
hohin728 <45099778+hohin728@users.noreply.github.com>
2021-05-13 23:51:21 +0800

    Start from ch. 5 UI and React

--- src/store.js
+++ src/store.js
@@ -1,23 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
-import rootReducer from './reducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import {
-  loggerMiddleware,
-  alwaysReturnHelloMiddleware,
-  delayedMessageMiddleware,
-  addCustomAttributeMiddleware,
-} from './exampleAddons/middleware'
+import rootReducer from './reducer'
 
-const middlewareEnhancer = applyMiddleware(
-  loggerMiddleware,
-  alwaysReturnHelloMiddleware,
-  delayedMessageMiddleware,
-  addCustomAttributeMiddleware
+const composedEnhancer = composeWithDevTools(
+  // Add whatever middleware you actually want to use here
+  applyMiddleware()
+  // other store enhancers if any
 )
 
-const composedEnhancer = composeWithDevTools(middlewareEnhancer)
-
-// Pass enhancer as the second arg, since there's no preloadedState
 const store = createStore(rootReducer, composedEnhancer)
-
 export default store
