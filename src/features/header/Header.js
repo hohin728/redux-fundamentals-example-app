2fa8067318ca99dbe92f3afac4b69d7f1a3d0195
hohin728 <45099778+hohin728@users.noreply.github.com>
2021-05-14 01:21:24 +0800

    Completed first half of ch. 5 UI and React

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,17 +1,32 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 const Header = () => {
   const [text, setText] = useState('')
+  const dispatch = useDispatch()
 
   const handleChange = (e) => setText(e.target.value)
+
+  const handleKeyDown = (e) => {
+    const trimmedText = e.target.value.trim()
+
+    if (e.key === 'Enter' && trimmedText) {
+      // Dispatch the "todo added" action with this text
+      dispatch({ type: 'todos/todoAdded', payload: trimmedText })
+      // And clear out the text input
+      setText('')
+    }
+  }
 
   return (
     <header className="header">
       <input
         className="new-todo"
         placeholder="What needs to be done?"
+        autoFocus={true}
         value={text}
         onChange={handleChange}
+        onKeyDown={handleKeyDown}
       />
     </header>
   )
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { Provider } from 'react-redux'
 
-import './api/server'
+import App from './App'
 import store from './store'
 
+import './index.css'
+
 ReactDOM.render(
+  // Render a `<Provider>` around the entire `<App>`,
+  // and pass the Redux store to as a prop
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
